393a87dec8e941f4141e1a419ce6ea8c7f10e37e
Alluminox <alluminox.devellopers@gmail.com>
2020-04-03 14:53:12 -0300

    Testing remote SERVERS

--- tcp-client.js
+++ tcp-client.js
@@ -1,48 +1,43 @@
 // Import net module.
 const net = require('net');
 
-// This function create and return a net.Socket object to represent TCP client.
-function getConn(){
+const client = new net.Socket();
 
-    const option = {
-        host:'localhost',
-        port: 9999
-    }
+client.connect({
+    host: '127.0.0.1',
+    port: 9999
+})
 
-    // Create TCP client.
-    const client = net.createConnection(option, function () {
-        console.log('[x] Client has been created');
-        console.log('[x] Client local connection address : ' + client.localAddress + ":" + client.localPort);
-        console.log('[x] Client remote connected at : ' + client.remoteAddress + ":" + client.remotePort);
+client.on('connect', () => {
+    console.log('Client: connection established with server');
+
+    console.log('---------client details -----------------');
+    var address = client.address();
+    var port = address.port;
+    var family = address.family;
+    var ipaddr = address.address;
+    console.log('Client is listening at port' + port);
+    console.log('Client ip :' + ipaddr);
+    console.log('Client is IP4/IP6 : ' + family);
+
+
+    // writing data to server
+    client.write(JSON.stringify(
+        {
+            port: 8080,
+            host: '127.0.0.1',
+            message: 'Registring into discovery'
+        }
+    ));
+
+    client.setEncoding('utf-8');
+
+    client.on('data',function(data){
+        console.log('[] Receiving data from server:' + data);
     });
 
-    client.setTimeout(1000);
-    client.setEncoding('utf8');
 
-    // When receive server send back data.
-    client.on('data', function (data) {
-        console.log('Server return data : ' + data);
-    });
-
-    // When connection disconnected.
-    client.on('end',function () {
-        console.log('Client socket disconnect. ');
-    });
-
-    client.on('timeout', function () {
-        console.log('Client connection timeout. ');
-        process.exit(0)
-    });
-
-    client.on('error', function (err) {
-        console.error(JSON.stringify(err));
-    });
-
-    return client;
-}
-
-
-
-// Create node client socket.
-var nodeClient = getConn();
-nodeClient.write('Node is more better than java. ');
+    setTimeout(function(){
+        client.end('Bye bye server');
+    },5000);      
+})
--- tcp-server.js
+++ tcp-server.js
@@ -1,47 +1,124 @@
 const net = require('net');
 
-const server = net.createServer(socket => {
+const server = net.createServer();
 
-    const { localAddress, localPort, remoteAddress, remotePort } = socket;
-    console.log('[x] Client connected');
-    console.log(`[x] Local client Connected ${localAddress}:${localPort}`);
-    console.log(`[x] Remote client Connected ${remoteAddress}:${remotePort}`)
-    
+server.on('close', () => console.log('Server has been closed!'))
+
+server.on('connection', socket => {
+
+    const serverAddress = server.address();
+    const { port, family, address } = serverAddress;
+
+    console.log('Server is litening on port: ', port);
+    console.log('Server Ip: ', address);
+    console.log('Server is Ip4/Ip6: ', family);
+
+    const { localPort, localAddress } = socket;
+    const { remoteAddress, remotePort, remoteFamily } = socket;
+
+    console.log('Server is listening at local port: ', localPort)
+    console.log('Server is listening at local address: ', localAddress)
+
+    console.log('Remote Socket is listening at port: ', remotePort)
+    console.log('Remote Socket ip: ', remoteAddress)
+    console.log('Remote Socker is IPV4/IPV6: ', remoteFamily);
+
+    server.getConnections((err, count) => {
+        if (err) {
+            console.log("Error at counting number of server connections")
+            return
+        }
+        console.log('Number of concurrent connections to the server', count);
+    })
+
+
     socket.setEncoding('utf-8');
-    socket.setTimeout(1000);
+    socket.setTimeout(8000, () => {
+        // called after timeout -> same as socket.on('timeout')
+        // it just tells that soket timed out => its ur job to end or destroy the socket.
+        // socket.end() vs socket.destroy() => end allows us to send final data and allows some i/o activity to finish before destroying the socket
+        // whereas destroy kills the socket immediately irrespective of whether any i/o operation is goin on or not...force destry takes place
+        console.log('Socket time out')
+    });
+
 
     socket.on('data', (data) => {
-        console.log('Client send data', data)
+        var bread = socket.bytesRead;
+        var bwrite = socket.bytesWritten;
+        console.log('Bytes read : ' + bread);
+        console.log('Bytes written : ' + bwrite);
+        console.log('Data sent to server : ' + data);
+
+        //echo data
+        var is_kernel_buffer_full = socket.write('Data ::' + data);
+        if(is_kernel_buffer_full){
+            console.log('Data was flushed successfully from kernel buffer i.e written successfully!');
+        }else{
+            socket.pause();
+        }
     });
 
-    socket.on('end', () => {
-        console.log("Client disconnected");
 
-        server.getConnections((err, count) => {
-            if (!err) console.log('[-] Total of connections now', count);
-            else console.error('[!] ERROR', JSON.stringify(err));
-        }) 
-    });
+    socket.on('drain',function(){
+        console.log('write buffer is empty now .. u can resume the writable stream');
+        socket.resume();
+      });
+      
+      socket.on('error',function(error){
+        console.log('Error : ' + error);
+      });
+      
+      socket.on('timeout',function(){
+        console.log('Socket timed out !');
+        socket.end('Timed out!');
+        // can call socket.destroy() here too.
+      });
+      
+      socket.on('end',function(data){
+        console.log('Socket ended from other end!');
+        console.log('End data : ' + data);
+      });
+      
+      socket.on('close',function(error){
+        var bread = socket.bytesRead;
+        var bwrite = socket.bytesWritten;
+        console.log('Bytes read : ' + bread);
+        console.log('Bytes written : ' + bwrite);
+        console.log('Socket closed!');
+        if(error){
+          console.log('Socket was closed coz of transmission error');
+        }
+      }); 
 
-    socket.on('timeout', () => {
-        console.log('Client request time out');
-
-    })
+      setTimeout(function(){
+        var isdestroyed = socket.destroyed;
+        console.log('Socket destroyed:' + isdestroyed);
+        socket.destroy();
+      },1200000);
 
 });
 
+
+
+// emits when any error occurs -> calls closed event immediately after this.
+server.on('close', function () {
+    console.log('TCP server socket is closed.');
+});
+
+server.on('error', function (error) {
+    console.error(JSON.stringify(error));
+});
+
+//emits when server is bound with server.listen
+server.on('listening',function(){
+console.log('Server is listening!');
+});
+
+server.maxConnections = 10;
 
 
 server.listen(9999, () => {
      // Get server address info.
     const serverInfo = server.address();
     console.log('%d TCP server has been started, listen address', Date.now(), JSON.stringify(serverInfo))
-
-    server.on('close', function () {
-        console.log('TCP server socket is closed.');
-    });
-
-    server.on('error', function (error) {
-        console.error(JSON.stringify(error));
-    });
 });
